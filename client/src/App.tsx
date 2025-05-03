69e0a33cb1299ca97806c0eee4256194db2ad21c
Schanze117 <aaronschanzenbach@gmail.com>
2025-05-03 10:21:23 -0600

    added apollo server to app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,40 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 import Navbar from './components/Navbar';
 
+// create an http link to connect to the GraphQL server
+ const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+//set up authentication link to include the token in the headers
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+}
+);
+// create an Apollo Client instance with the http link and authentication link
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+// The App component wraps the entire application with ApolloProvider to provide the Apollo Client instance to the rest of the app
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
